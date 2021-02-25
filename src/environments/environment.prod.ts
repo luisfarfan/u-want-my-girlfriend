ca9b3a9207edafec6fd08aea11aaa98c2f0b3d0b
Luis Eduardo Farfan Melgar <Luis Eduardo Farfan Melgar>
2021-02-25 13:33:50 -0500

    chore: config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  SERVICE_ID: 'service_4lk123e',
+  TEMPLATE_ID: 'template_dia39bf',
+  user_id: 'user_dIheZov4wsHz15EcnDVwm',
+  token: 'af02677996c961f38ff68f5c29bb5565'
 };
