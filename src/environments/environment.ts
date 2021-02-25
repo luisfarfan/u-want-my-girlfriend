002dd5572ab36f3b5b1f4d4d562082b15899f442
Luis Eduardo Farfan Melgar <Luis Eduardo Farfan Melgar>
2021-02-25 13:10:02 -0500

    app done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  SERVICE_ID: 'service_4lk123e',
+  TEMPLATE_ID: 'template_dia39bf',
+  user_id: 'user_dIheZov4wsHz15EcnDVwm',
+  token: 'af02677996c961f38ff68f5c29bb5565'
 };
 
 /*
